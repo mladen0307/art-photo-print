c976e5939172af1c65d053c5d550d4b07ff8f6f9
mladen0307 <57376401+mladen0307@users.noreply.github.com>
2022-11-07 14:11:22 +0100

    Prep for Cyclic deployment

---
+++ client/build/precache-manifest.8e3824ce03c8b9de1a34914cf0e46b4a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "413eb81ab2c476289af6a735efbef0e4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b2391169a2b9ce3116c0",
+    "url": "./static/css/2.63886832.chunk.css"
+  },
+  {
+    "revision": "b2391169a2b9ce3116c0",
+    "url": "./static/js/2.38995b2c.chunk.js"
+  },
+  {
+    "revision": "fa96ded61e1d8337a7017d02ff86cf53",
+    "url": "./static/js/2.38995b2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d254fea7d6f4bc857a4b",
+    "url": "./static/js/main.c942a53e.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.c942a53e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25ec3f859107274f79d9",
+    "url": "./static/js/runtime-main.95d10083.js"
+  },
+  {
+    "revision": "f0469ab5b90a324df5fdedc21d552304",
+    "url": "./static/media/image_upload.f0469ab5.png"
+  },
+  {
+    "revision": "2642b879f5d37b4f04a0d6fae0ed950b",
+    "url": "./static/media/instagram.2642b879.png"
+  },
+  {
+    "revision": "80e331ba3e832186f32c72b056dc62ca",
+    "url": "./static/media/logos.80e331ba.png"
+  },
+  {
+    "revision": "be2cc86764ab788b282afbc2ecc70260",
+    "url": "./static/media/photoprintbanner.be2cc867.png"
+  },
+  {
+    "revision": "3cbcf04d38eb498fbffec631af79636a",
+    "url": "./static/media/print-sizes-min.3cbcf04d.png"
+  },
+  {
+    "revision": "a437d47114e14262d87f28ac68a59efe",
+    "url": "./static/media/saved_success.a437d471.png"
+  }
+]);
