43810eb6a383099da34ad791547fb46abdd781b4
mladen0307 <mladen.k1234@gmail.com>
2026-04-06 18:01:02 +0200

    promena cena v3

---
+++ client/build/precache-manifest.e21f4aca2345f4c9d113f2bbdd8d20ea.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a7480fa8879f7842512480e091b6af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "98be19d4573d83d285fb",
+    "url": "./static/css/2.63886832.chunk.css"
+  },
+  {
+    "revision": "98be19d4573d83d285fb",
+    "url": "./static/js/2.611b5ed9.chunk.js"
+  },
+  {
+    "revision": "fa96ded61e1d8337a7017d02ff86cf53",
+    "url": "./static/js/2.611b5ed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4285624e6f9c9cdffa3f",
+    "url": "./static/js/main.52f1a9f8.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.52f1a9f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25ec3f859107274f79d9",
+    "url": "./static/js/runtime-main.95d10083.js"
+  },
+  {
+    "revision": "f0469ab5b90a324df5fdedc21d552304",
+    "url": "./static/media/image_upload.f0469ab5.png"
+  },
+  {
+    "revision": "2642b879f5d37b4f04a0d6fae0ed950b",
+    "url": "./static/media/instagram.2642b879.png"
+  },
+  {
+    "revision": "80e331ba3e832186f32c72b056dc62ca",
+    "url": "./static/media/logos.80e331ba.png"
+  },
+  {
+    "revision": "be2cc86764ab788b282afbc2ecc70260",
+    "url": "./static/media/photoprintbanner.be2cc867.png"
+  },
+  {
+    "revision": "f4e71c39672024ea0b11cb2fd27212e4",
+    "url": "./static/media/print-sizes-min.f4e71c39.png"
+  },
+  {
+    "revision": "a437d47114e14262d87f28ac68a59efe",
+    "url": "./static/media/saved_success.a437d471.png"
+  }
+]);
